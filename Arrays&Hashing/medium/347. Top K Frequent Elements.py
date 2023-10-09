8131a1f3d75804871610ead8b364df5b4e6c80b9
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-10-09 11:59:00 +0600

    1 medium solved but could not implement my solution to the code

--- Arrays&Hashing/medium/347. Top K Frequent Elements.py
+++ Arrays&Hashing/medium/347. Top K Frequent Elements.py
@@ -1,12 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        set_number = set()
-        
+        # Step 1: Create a dictionary to store the frequency of each number.
+        num_freq = {}
+        for num in nums:
+            # If the number is already in the dictionary, increment its frequency.
+            if num in num_freq:
+                num_freq[num] += 1
+            else:
+                # If the number is not in the dictionary, add it with a frequency of 1.
+                num_freq[num] = 1
 
-# Create an instance of the Solution class
-solution = Solution()
+        # Step 2: Create a list of unique numbers from the dictionary keys.
+        unique_nums = list(num_freq.keys())
 
+        # Step 3: Sort the unique_nums list based on the frequency (in descending order).
+        # We use a lambda function as the key for sorting, which retrieves the frequency of each number from num_freq.
+        # Sorting in reverse order ensures that the most frequent numbers come first.
+        unique_nums.sort(key=lambda x: num_freq[x], reverse=True)
 
-# Sample input 1
-nums1 = [1, 1, 1, 2, 2, 3]
-k1 = 2
+        # Step 4: Create the result list with the top k most frequent elements.
+        result = []
+        for i in range(k):
+            # Append the first k elements from the sorted unique_nums list to the result.
+            result.append(unique_nums[i])
+
+        return result
--- test.py
+++
@@ -1,6 +0,0 @@
-import random 
-secret_num = random.randint(0, 10)
-passed_it = False 
-print('I am thinking of a number')
-while not guessed_it:
-   
