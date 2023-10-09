34015d0e4bbf9435cca01b2424b949a3b6b96650
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-10-09 15:46:52 +0600

    solved

--- Arrays&Hashing/easy/1. Two Sum.py
+++ Arrays&Hashing/easy/1. Two Sum.py
@@ -1,36 +1,36 @@
-from typing import List
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+# from typing import List
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
 
-# Sample input
-nums = [2, 7, 11, 15]
-target = 9
+# # Sample input
+# nums = [2, 7, 11, 15]
+# target = 9
 
-solution = Solution()
+# solution = Solution()
 
-result = solution.twoSum(nums, target)
-
-print(result)
-
-
-
-
-
-# target = int(input())
-# num_list = [1, 2, 3, 4, 1, 1, 1, 1, 65, 76, 87, 87, 98, 8]
-# my_dict = {}  
-# result = []
-
-# for index, element in enumerate(num_list):
-#     # my_dict[index] = element
-#     pair = target - element
-#     if pair in my_dict:
-#         result.append((my_dict[pair], index))
-#     my_dict[element] = index
+# result = solution.twoSum(nums, target)
 
 # print(result)
+
+
+
+
+
+target = int(input())
+num_list = [1, 2, 3, 4, 1, 1, 1, 1, 65, 76, 87, 87, 98, 8]
+my_dict = {}  
+result = []
+
+for index, element in enumerate(num_list):
+    # my_dict[index] = element
+    pair = target - element
+    if pair in my_dict:
+        result.append((my_dict[pair], index))
+    my_dict[element] = index
+
+print(result)
