895d4eb26e55e068dbdd26c4c6b457eb60e2297c
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-10-17 18:43:56 +0600

    another solution

--- Two pointers/easy/125. Valid Palindrome.py
+++ Two pointers/easy/125. Valid Palindrome.py
@@ -28,3 +28,26 @@
 #             else: 
 #                 return False
 #         return True
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1 
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    def alphaNum(self, c):
+        return (
+            'A' <= c <= 'Z' or 
+            'a' <= c <= 'z' or 
+            '0' <= c <= '9'
+        )
