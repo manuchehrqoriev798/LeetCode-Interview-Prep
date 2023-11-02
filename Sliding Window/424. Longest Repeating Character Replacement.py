c495bb355c7f0300f4d914f05a544adfc1d45d1b
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-02 17:46:23 +0600

    longest repeating character

---
+++ Sliding Window/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        charCount = {}  
+        maxCount = 0
+        res = 0
+        for r in range(len(s)):
+            charCount[s[r]] = charCount.get(s[r], 0) + 1  
+            maxCount = max(maxCount, charCount[s[r]])
+
+            while (r - l + 1 - maxCount) > k:
+                charCount[s[l]] -= 1  
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
