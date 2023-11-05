24a16b1dd10211f9b9a7dc3dc9f43ff886338e56
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-05 18:07:14 +0600

    206

--- Linked list/206. Reverse Linked List.py
+++ Linked list/206. Reverse Linked List.py
@@ -6,18 +6,26 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize pointers to keep track of the previous and current nodes
         prev = None
         current = head
         
         while current is not None:
-            # Store the next node temporarily
             next_node = current.next
-            # Reverse the current node's pointer to the previous node
             current.next = prev
-            # Move the prev and current pointers one step forward
             prev = current
             current = next_node
         
-        # At the end, 'prev' will be pointing to the new head of the reversed list
         return prev
+
+
+
+        # if not head:
+        #     return None
+        
+        # newHead = head
+        # if head.next:
+        #     newHead = self.reverseList(head.next)
+        #     head.next.next = head
+        # head.next = None
+
+        # return newHead
---
+++ Linked list/21. Merge Two Sorted Lists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        if list1 is not None:
+            tail.next = list1
+        else:
+            tail.next = list2
+        
+        return dummy.next
