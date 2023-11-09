a7c5bdadc59f9dd97e86b391e114f8ca596c2b3a
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-09 14:14:31 +0600

    updated

--- Trees/104. Maximum Depth of Binary Tree.py
+++ Trees/104. Maximum Depth of Binary Tree.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        # # Recursive DFS
-        # if not root:
-        #     return 0
-        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # Recursive DFS
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
         
         # # BFS
