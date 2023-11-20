f47b463482da730f05a294d9f07fa4d19d9df318
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-20 14:45:08 +0600

    updated

---
+++ Archive/Arrays&Hashing/238. Product of Array Except Self.py
@@ -0,0 +1,63 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Using division and also it is slow
+        # total = 1
+        # zero_count = 0
+
+        # for num in nums:
+        #     if num == 0:
+        #         zero_count += 1
+        #         continue
+        #     else:
+        #         total *= num
+
+        # res = []
+        # for num in nums:
+        #     if zero_count > 1:
+        #         res.append(0)
+        #     elif zero_count == 1 and num == 0:
+        #         res.append(total)
+        #     elif zero_count == 0:
+        #         res.append(total // num)
+        #     else:
+        #         res.append(0)
+
+        # return res
+        
+        
+        
+        
+        # n = len(nums)
+        
+        # pre, post = 1, 1
+        # pr = [0 for i in range(n)]
+        # pst = [0 for i in range(n)]
+        
+        # for i in range(n):
+        #     pr[i] = pre
+        #     pre *= nums[i]
+            
+        # for i in range(n - 1, -1, -1):
+        #     pst[i] = post
+        #     post *= nums[i]
+            
+        # ans = [0 for i in range(n)]
+        # for i in range(n):
+        #     ans[i] = pr[i] * pst[i]
+            
+        # return ans
+        
+        
+        res = [1] * len(nums)
+        prefix = 1
+        postfix = 1
+
+        for i in range(len(nums)):
+            res[i] *= prefix
+            prefix *= nums[i]
+        
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
