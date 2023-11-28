270d01521280ac67e4d1976659170f6a1d9a3f25
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-28 10:37:00 +0600

    updated

--- Archive/Binary Search/DidNotGetIt.875. Koko Eating Bananas.py
+++ Archive/Binary Search/DidNotGetIt.875. Koko Eating Bananas.py
@@ -1,15 +1,38 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 0, max(piles)
+        l, r = ceil(sum(piles)/h), max(piles)
         res = r
         while l <= r:
-            k = (l+r)//2
-            hours = 0
-            for p in piles: 
-                hours += math.ceil(p/k)
-            if hours <= h:
-                res = min(res, k)
-                r = k -1
-            elif hours >= h:
+            k = l + (r - l) // 2
+            hrs = 0
+            for pile in piles:
+                hrs += ceil(pile / k)
+                
+            if hrs > h:
                 l = k + 1
+            else:
+                res = k
+                r = k - 1
+        
         return res
+
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        while left < right:
+            k = (left + right) // 2
+            total_hours = 0
+            for pile in piles:
+                total_hours += ceil(pile / k)
+                if total_hours > h:
+                    break
+
+            if total_hours <= h:
+                right = k
+            else:
+                left = k + 1
+
+        return right
