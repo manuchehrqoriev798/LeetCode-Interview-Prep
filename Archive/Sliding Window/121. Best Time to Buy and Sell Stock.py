e2f410e0c1d17ad5524f68097bf3d5d4961b9d10
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-11-30 15:42:18 +0600

    updated

--- Archive/Sliding Window/121. Best Time to Buy and Sell Stock.py
+++ Archive/Sliding Window/121. Best Time to Buy and Sell Stock.py
@@ -14,3 +14,88 @@ class Solution:
             r += 1
 
         return max_profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Initialize left and right pointers for iterating through the prices.
+        l, r = 0, 1
+        # Initialize the variable to store the maximum profit.
+        profit = 0
+
+        # Iterate through the prices using the right pointer.
+        while r < len(prices):
+            # Check if selling at the current right pointer position would yield a positive profit.
+            if prices[r] - prices[l] > 0:
+                # Update the maximum profit if the current profit is greater.
+                profit = max(profit, prices[r] - prices[l])
+            else:
+                # If selling at the current right pointer position results in a loss or no gain,
+                # update the left pointer to the current right pointer position.
+                l = r
+            # Move the right pointer to the next position.
+            r += 1
+        
+        # Return the maximum profit obtained.
+        return profit
+
+
+
+
+# Another implementation of the same problem using a different approach.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Initialize variables to keep track of the minimum and maximum prices,
+        # as well as the maximum profit.
+        mini = prices[0]
+        maxi = prices[0]
+        prof = 0
+
+        # Iterate through the prices.
+        for i in prices:
+            # Check if the current price is lower than the minimum price.
+            if i < mini:
+                # Update the maximum profit if the difference between maximum and minimum is greater.
+                prof = max(maxi - mini, prof)
+                # Update the minimum and maximum prices to the current price.
+                mini = i
+                maxi = i
+            # Check if the current price is higher than the maximum price.
+            elif i > maxi:
+                # Update the maximum price to the current price.
+                maxi = i
+        
+        # Return the maximum profit, considering the last segment of prices.
+        return max(maxi - mini, prof)
+
+
+
+
+
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        # Check if the list of prices has less than 2 elements
+        if len(prices) < 2:
+            # If so, there's no opportunity to make a profit, return 0
+            return 0
+
+        # Initialize variables to track maximum profit and minimum price
+        maxProfit = 0
+        minPrice = prices[0]
+
+        # Iterate through prices starting from the second element
+        for price in prices[1:]:
+            # Update the maximum profit by taking the maximum of the current maxProfit
+            # and the difference between the current price and the minimum price so far
+            maxProfit = max(maxProfit, price - minPrice)
+            
+            # Update the minimum price to be the minimum of the current price and the
+            # minimum price so far
+            minPrice = min(price, minPrice)
+
+        # Return the maximum profit
+        return maxProfit
+
+
--- Archive/Sliding Window/3. Longest Substring Without Repeating Characters.py
+++ Archive/Sliding Window/3. Longest Substring Without Repeating Characters.py
@@ -10,3 +10,51 @@ class Solution:
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
+
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i = 0
+        j = 0
+        n = len(s)
+        res = ""
+        ans = 0
+        while j<n and i<=j:
+            if s[j] not in res:
+                res += s[j]
+                j += 1
+                ans = max(ans , len(res))
+
+            else:
+                while s[j] in res:
+                    
+                    res = res[1:]
+                    i+=1
+                # ans = max(ans , len(res))
+                res += s[j]
+                j += 1
+        return ans
+
+
+
+
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        l = 0
+        r = 0
+        char = set()
+        res = 0
+        
+        while r < len(s):
+            while (s[r] in char):
+                char.remove(s[l])
+                l += 1
+            char.add(s[r])
+            res = max(res,r-l+1)
+            r += 1
+        
+        return res
