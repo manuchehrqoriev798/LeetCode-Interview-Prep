5484f98ca9f4e4c141220b20c03effd2e9a01a15
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-04 12:28:05 +0600

    updated

--- Archive/Arrays&Hashing/49. Group Anagrams.py
+++ Archive/Arrays&Hashing/49. Group Anagrams.py
@@ -1,39 +1,55 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # # Initialize a regular dictionary
-        # res = {}
+        # Initialize a regular dictionary
+        res = {}
 
-        # # Iterate through each word in the input list
-        # for word in strs:
-        #     # Initialize a list to represent the count of each character in the current word
-        #     count = [0] * 26
-
-        #     # Count the occurrences of each character in the current word
-        #     for char in word:
-        #         count[ord(char) - ord('a')] += 1
-
-        #     # Convert the count list to a tuple and use it as a key to group anagrams
-        #     key = tuple(count)
-
-        #     # Check if the key exists in the dictionary
-        #     if key in res:
-        #         res[key].append(word)
-        #     else:
-        #         # If the key doesn't exist, initialize a new list for that key
-        #         res[key] = [word]
-
-        # # Return the values (groups of anagrams) from the dictionary
-        # return list(res.values())
-
-
-        res = []
-        anagram = {}
+        # Iterate through each word in the input list
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            if sorted_word in anagram:
-                anagram[sorted_word].append(word)
+            # Initialize a list to represent the count of each character in the current word
+            count = [0] * 26
+
+            # Count the occurrences of each character in the current word
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+
+            # Convert the count list to a tuple and use it as a key to group anagrams
+            key = tuple(count)
+
+            # Check if the key exists in the dictionary
+            if key in res:
+                res[key].append(word)
             else:
-                anagram[sorted_word] = [word]
-        for key in anagram:
-            res.append(anagram[key])
-        return res
+                # If the key doesn't exist, initialize a new list for that key
+                res[key] = [word]
+
+        # Return the values (groups of anagrams) from the dictionary
+        return list(res.values())
+
+
+ class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_dict = {}
+
+        result = []
+
+        for word in strs:
+
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word not in anagram_dict:
+                anagram_dict[sorted_word] = []
+            
+            anagram_dict[sorted_word].append(word)
+        
+        result = list(anagram_dict.values())
+
+        return result
+
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            d[tuple(sorted(s))].append(s)
+        return d.values()
--- test.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-test
-=======
-lkjkljkjl
->>>>>>> 5480f17 (test)
