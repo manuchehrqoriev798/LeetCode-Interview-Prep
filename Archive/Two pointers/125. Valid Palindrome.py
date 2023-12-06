2a64d4a98f2771ab8e68f5a7f1d8558230670da4
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-06 17:24:42 +0600

    new solution added

--- Archive/Two pointers/125. Valid Palindrome.py
+++ Archive/Two pointers/125. Valid Palindrome.py
@@ -30,6 +30,24 @@
 #         return True
 
 
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
+#         l, r = 0, len(s) - 1
+
+#         while l <= r:
+#             if not s[l].isalnum():
+#                 l += 1
+#             elif not s[r].isalnum():
+#                 r -= 1
+#             else:
+#                 if s[l] != s[r]:
+#                     return False
+#                 l += 1
+#                 r -= 1
+
+#         return True
+
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
