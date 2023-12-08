4d0746e2b6607cb0043dfc1a1b815317c3eefd63
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-08 09:14:01 +0600

    another new solution added

--- Archive/1475. Final Prices With a Special Discount in a Shop.py
+++ Archive/1475. Final Prices With a Special Discount in a Shop.py
@@ -12,3 +12,17 @@ class Solution:
                     break
             res.append(prices[i] - discount)
         return res
+
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(len(prices)):
+            for j in range(i+1,len(prices)):
+                if prices[j] <= prices[i] and j>i:
+                    prices[i]-=prices[j]
+                    break
+
+        return prices
+
+        
