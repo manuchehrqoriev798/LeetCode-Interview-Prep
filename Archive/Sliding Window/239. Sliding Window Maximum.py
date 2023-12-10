049a07e5cb4a4e28cc110a9a2efedd53c7daf630
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-10 19:38:35 +0600

    the first solution added

---
+++ Archive/Sliding Window/239. Sliding Window Maximum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSlidingWindow(self, nums, k: int):
+        l = 0
+
+        res = []
+
+        for r in range(0, len(nums) - k + 1):
+            maximum = max(nums[l:(r + k)])
+            res.append(maximum)
+            l += 1
+        
+        return res
--- Archive/Sliding Window/76. Minimum Window Substring.py
+++ Archive/Sliding Window/76. Minimum Window Substring.py
@@ -1,3 +1,53 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Check if t is an empty string, return an empty string if true
+        if t == "":
+            return ""
+
+        # Initialize dictionaries to store the count of characters in t and the current window
+        countT, window = {}, {}
+
+        # Count the occurrences of each character in t and store in countT dictionary
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        # Variables to keep track of the characters needed and currently have in the window
+        have, need = 0, len(countT)
+
+        # Variables to store the result (window indices) and its length
+        res, resLen = [-1, -1], float("infinity")
+        
+        # Initialize the left pointer of the window
+        left = 0
+
+        # Iterate over the characters in the input string s
+        for right in range(len(s)):
+            # Update the count of the current character in the window
+            window[s[right]] = 1 + window.get(s[right], 0)
+
+            # Check if the current character is in t and the window count matches the target count
+            if s[right] in countT and window[s[right]] == countT[s[right]]:
+                have += 1
+
+            # Check if all characters in t are present in the current window
+            while have == need:
+                # Update the result if the current window is smaller than the previous result
+                if (right - left + 1) < resLen:
+                    res = [left, right]
+                    resLen = (right - left + 1)
+
+                # Move the left pointer to shrink the window
+                window[s[left]] -= 1
+
+                # Update the 'have' count if the leftmost character in the window is in t
+                if s[left] in countT and window[s[left]] < countT[s[left]]:
+                    have -= 1
+
+                # Move the left pointer to the right
+                left += 1
+
+        # Return the substring from the result indices or an empty string if no valid window is found
+        return s[res[0]: res[1] + 1] if resLen != float("infinity") else ""
 
 
 
