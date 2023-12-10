b08d038ffac2e42d5e7007e40e1bd58e9a222bfc
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-10 21:04:05 +0600

    updated

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-#Writing answers to notebook to remember better 
-#Finished writing odia of answers to the Notebook 
+# Writing solution of the solved problems in my notebook.
+# So that after a while if i could not solve the problem look at the notebook but not to the solution part.
+# I assume it is better than not writing answers at all.
