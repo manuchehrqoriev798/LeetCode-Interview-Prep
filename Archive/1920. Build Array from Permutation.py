f6d80d7206d5416dc209942e39f540f544beb3e6
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-11 17:41:49 +0600

    same solution with shorter solution

--- Archive/1920. Build Array from Permutation.py
+++ Archive/1920. Build Array from Permutation.py
@@ -7,3 +7,20 @@ class Solution:
             ans[i] = nums[nums[i]]  
 
         return ans
+
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+    
+
+
+
+
+
+
+
