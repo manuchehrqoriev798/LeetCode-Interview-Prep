c8cea2dabab0f339bbfa5f85fd1cb879777ade97
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-12 14:32:14 +0600

    two more solution are added but i understand fully only the first one

--- Archive/Linked list/206. Reverse Linked List.py
+++ Archive/Linked list/206. Reverse Linked List.py
@@ -25,13 +25,29 @@ class Solution:
 
 
 
-        # if not head:
-        #     return None
-        
-        # newHead = head
-        # if head.next:
-        #     newHead = self.reverseList(head.next)
-        #     head.next.next = head
-        # head.next = None
 
-        # return newHead
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
+
+
+
+
+
+
+
+class Solution:
+    def reverseList(self, head):
+        slow, fast = None, head
+        while fast:
+            fast.next, slow, fast = slow, fast, fast.next
+        return slow
