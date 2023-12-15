64ea8811b7fe87e4168e7bb0d650bfd2a71fa350
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-15 11:49:07 +0600

    antoher problem solvede Outside of Neetcode 150

---
+++ Archive/2469. Convert the Temperature.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.80 + 32.00 ]
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        return [kelvin, fahrenheit]
+
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        return [round(kelvin, 5), round(fahrenheit, 5)]
