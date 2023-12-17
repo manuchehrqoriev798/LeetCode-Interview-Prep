8e87d6c868674f1d572448e42510e21f1cc8ccbf
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-17 15:18:25 +0600

    test.py changed to todo.py

--- todo.py
+++ todo.py
@@ -1,2 +1,14 @@
-Motivation is temporarily, have a discipline
+Motivation is temporarily, have a discipline !
 Don't search for the right choice, make the choice right !!!
+
+
+# TODO:
+1. Stack
+2. Tree
+3. Backtracking
+4. Graph
+
+5. Easy Marathone
+6. Medium Marathone
+
+5. Yandex Problems
