65bfdc40012d707db7372990441e47d5d51ddec2
Manuchehr Qoriev <112572372+manuchehrqoriev798@users.noreply.github.com>
2023-12-17 13:58:54 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-# Writing solution of the solved problems in my notebook.
-# So that after a while if i could not solve the problem look at the notebook but not to the solution part.
-# I assume it is better than not writing answers at all.
-
-# Got lost after all this AI stuff. Can i become developer? Will i find work?
-# I have realized that trying is better than not trying at all.
-
-# Quoted myself one of the best quotes (then realized that it has been been said by another person)
-
+Motivation is temporarily, have a discipline
 Don't search for the right choice, make the choice right !!!
