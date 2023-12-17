57d77a76ce78c707fe548157cb5df6b1b9214b2c
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-17 16:45:01 +0600

    Solved but it is little bit hard for me now to understand since i am not comfortable with  problems which requare multiple functions to solve

--- Archive/Linked list/23. Merge k Sorted Lists.py
+++ Archive/Linked list/23. Merge k Sorted Lists.py
@@ -1,3 +1,42 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+
         
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                if i + 1 < len(lists):
+                    l2 = lists[i + 1]
+                else:
+                    l2 = None
+                mergedLists.append(self.mergedTwoLists(l1, l2))
+            
+            lists = mergedLists
+        
+        return lists[0]
+        
+
+    def mergedTwoLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+
+            tail = tail.next
+        
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+        
+        return dummy.next
