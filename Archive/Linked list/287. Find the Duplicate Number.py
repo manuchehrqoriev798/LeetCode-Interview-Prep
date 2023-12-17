26f199437384ca03bfa7ca0b1cf7d28a552b95bb
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-17 15:14:42 +0600

    saved

--- Archive/Linked list/287. Find the Duplicate Number.py
+++ Archive/Linked list/287. Find the Duplicate Number.py
@@ -20,61 +20,71 @@ class Solution:
                 return fast
 
 
-        # # Solving with constant extra space - Floyd's algorithm
 
-        # # Initialize two pointers, slow and fast, both starting at the beginning of the array.
-        # slow, fast = 0, 0
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Solving with constant extra space - Floyd's algorithm
 
-        # # Start a loop that continues until a cycle is detected in the array.
-        # while True:
-        #     # Move slow one step ahead.
-        #     slow = nums[slow]
-        #     # Move fast two steps ahead.
-        #     fast = nums[nums[fast]]
+        # Initialize two pointers, slow and fast, both starting at the beginning of the array.
+        slow, fast = 0, 0
 
-        #     # If slow and fast meet at the same value, a cycle is detected, so exit the loop.
-        #     if slow == fast:
-        #         break
+        # Start a loop that continues until a cycle is detected in the array.
+        while True:
+            # Move slow one step ahead.
+            slow = nums[slow]
+            # Move fast two steps ahead.
+            fast = nums[nums[fast]]
 
-        # # After exiting the first loop, initialize a new pointer, slow2, at the beginning of the array.
-        # slow2 = 0
+            # If slow and fast meet at the same value, a cycle is detected, so exit the loop.
+            if slow == fast:
+                break
 
-        # # Start a new loop to find the actual duplicate value.
-        # while True:
-        #     # Move slow2 one step ahead.
-        #     slow2 = nums[slow2]
-        #     # Move slow one step ahead.
-        #     slow = nums[slow]
+        # After exiting the first loop, initialize a new pointer, slow2, at the beginning of the array.
+        slow2 = 0
 
-        #     # When slow2 and slow meet at the same value, it indicates the duplicate value, so return it.
-        #     if slow2 == slow:
-        #         return slow
+        # Start a new loop to find the actual duplicate value.
+        while True:
+            # Move slow2 one step ahead.
+            slow2 = nums[slow2]
+            # Move slow one step ahead.
+            slow = nums[slow]
+
+            # When slow2 and slow meet at the same value, it indicates the duplicate value, so return it.
+            if slow2 == slow:
+                return slow
 
 
 
 
+
+
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
         # Using set
-        # setNums = set()
-        # for i in range(nums):
-        #     if nums[i] not in setNums:
-        #         setNums.add(nums[i])
-        #     else:
-        #         return i
-        # return -1
+        setNums = set()
+        for i in range(nums):
+            if nums[i] not in setNums:
+                setNums.add(nums[i])
+            else:
+                return i
+        return -1
 
 
 
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Using Hashmap 
+        num_counts = {}
+        for num in nums:
+            if num in num_counts:
+                num_counts[num] += 1
+            else:
+                num_counts[num] = 1
 
-        # # Using Hashmap 
-        # num_counts = {}
-        # for num in nums:
-        #     if num in num_counts:
-        #         num_counts[num] += 1
-        #     else:
-        #         num_counts[num] = 1
+        for num, count in num_counts.items():
+            if count > 1:
+                return num
 
-        # for num, count in num_counts.items():
-        #     if count > 1:
-        #         return num
-
-        # return -1
+        return -1
