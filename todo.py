ae7f49c2cd6438d22ec93a29662d3116bfe402a6
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-27 22:50:58 +0600

    updated the whole plan

--- todo.py
+++ todo.py
@@ -2,6 +2,9 @@ Motivation is temporarily, have a discipline !
 Don't search for the right choice, make the choice right !!!
 
 
+
+You should understand what happens if any line of code will be removed
+
 # TODO:
 1. Stack. 2 problems solved
 2. Tree
@@ -22,3 +25,31 @@ From 27th to 30th: Stack
 From 1st to 4th: Tree
 From 5th to 8th: Backtracking
 From 9th to 12th: Graph
+
+From 13th to 15th: Easy Marathone
+From 16th to 20th: Medium Marathone
+
+
+TODO topics:
+- Two Pointers
+- Binary Search
+- Sliding Window
+- Trees (DFS, BFS)
+- Merge Intervals
+- Backtracking
+- Stacks
+- Hash Maps
+- Heaps
+- Linked Lists, specifically creating new ones and reversing existing
+- Greedy
+- Fast and Slow Pointers
+- Two Heaps
+- K-way merge
+- Top K Elements
+- Subsets
+- Matrices
+- Topological Sort
+- Dynamic Programming
+- Tries
+- Union Find
+- Bitwise manipulation
