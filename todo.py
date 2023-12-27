38fba28941f2225c0a22c632b93df07685eb4ed5
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-27 13:05:04 +0600

    saved

--- todo.py
+++ todo.py
@@ -12,3 +12,13 @@ Don't search for the right choice, make the choice right !!!
 6. Medium Marathone
 
 5. Yandex Problems
+
+
+
+
+
+#TODO in details:
+From 27th to 30th: Stack
+From 1st to 4th: Tree
+From 5th to 8th: Backtracking
+From 9th to 12th: Graph
