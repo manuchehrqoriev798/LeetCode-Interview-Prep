5d6ce65ce355665c0848bac202f124b4d8ee7cdc
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-30 12:32:00 +0600

    solved

--- Neetcode 150/Trees/226. Invert Binary Tree.py
+++ Neetcode 150/Trees/226. Invert Binary Tree.py
@@ -1,11 +1,15 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # check basecase
         if root is None:
             return None
-
+        
+        # swap
         tmp = root.left
         root.left = root.right
         root.right = tmp
+        
+        # call recursive
         self.invertTree(root.left)
         self.invertTree(root.right)
 
