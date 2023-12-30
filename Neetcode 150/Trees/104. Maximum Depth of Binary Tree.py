be77ea47668393d2b5e9f74178cb245e0de7afa8
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-30 13:22:04 +0600

    solved in 3 difirent ways but still there are some gaps in knowledge

--- Neetcode 150/Trees/104. Maximum Depth of Binary Tree.py
+++ Neetcode 150/Trees/104. Maximum Depth of Binary Tree.py
@@ -1,50 +1,52 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
         # Recursive DFS
+        
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+
+
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # BFS
+
+        if not root:
+            return 0
+        level = 0
+        # q = [root]  # list
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                # node = q.pop(0)  # list
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+
+
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Iterative DFS
         
-        # # BFS
-        # if not root:
-        #     return 0
-        # level = 0   
-        # q = deque([root])
-        # while q:
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        #     level += 1
-        # return level
+        if not root:
+            return 0
 
-
-        # # Iterative DFS
-        # if not root:
-        #     return 0
-        # res = 1
-        # stack = [[root, 1]]
-        # while stack:
-        #     node, depth = stack.pop()
-        #     if node:
-        #         res = max(res, depth)
-        #         stack.append([node.left, depth + 1])
-        #         stack.append([node.right, depth + 1])
-        # return res
-
-
-        # # Iterative DFS
-        # stack = [[root, 1]]
-        # res = 0
-        # while stack:
-        #     node, depth = stack.pop()
-        #     if node:
-        #         res = max(res, depth)
-        #         stack.append([node.left, depth + 1])
-        #         stack.append([node.right, depth + 1])
-        # return res
-
+        level = 1
+        
+        stack = [[root, 1]]
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                level = max(level, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return level
