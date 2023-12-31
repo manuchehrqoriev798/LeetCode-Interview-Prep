9f51247ecd8ffa946e67da3035fb29133d89bc4b
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-12-31 14:49:29 +0600

    got lost even more

--- Neetcode 150/Trees/543. Diameter of Binary Tree.py
+++ Neetcode 150/Trees/543. Diameter of Binary Tree.py
@@ -1,32 +1,58 @@
-# TODO: revise
+# TODO revise
+# The diameter of a tree is the number of nodes on the longest path between two leaves in the tree.
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # Initialize a list to store the maximum diameter found so far
         res = [0]
 
-        # Define a helper function for depth-first search
         def dfs(root):
-            # If the tree is empty, return -1
+            # Check if the current node is empty (base case)
             if not root:
-                return -1 
-            # Recursively find the maximum diameter of the left subtree
+                return 0
+            # Recursively calculate the height of the left subtree
             left = dfs(root.left)
-            # Recursively find the maximum diameter of the right subtree
+            # Recursively calculate the height of the right subtree
             right = dfs(root.right)
             # Update the maximum diameter found so far. The diameter at a node is 
             # the sum of the heights of its left and right subtrees plus 2 (for the 
             # two edges connecting the node to the longest paths in its subtrees)
             res[0] = max(res[0], left + right + 2)
-            # becasue the hight of the Nul tree is -1 and in order to cancel it we have to put +1 to make it 0.
+            # Because the height of the Null tree is -1, and to cancel it, we add 1 to make it 0.
             return 1 + max(left, right)
         
-        # Call the helper function on the root of the tree
+        # Start the depth-first search from the root
         dfs(root)
-        # Return the maximum diameter found
+        # Return the maximum diameter found during the traversal
         return res[0]
 
 
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # Initialize a variable to store the maximum diameter found so far
+        res = 0
+
+        def dfs(root):
+            # Use 'nonlocal' to indicate that 'res' refers to the variable in the outer scope
+            nonlocal res
+            # Check if the current node is empty (base case)
+            if not root:
+                return 0
+
+            # Recursively calculate the height of the left subtree
+            left = dfs(root.left)
+            # Recursively calculate the height of the right subtree
+            right = dfs(root.right)
+            # Update the maximum diameter found so far. The diameter at a node is 
+            # the sum of the heights of its left and right subtrees
+            res = max(res, left + right)
+            # Because the height of the Null tree is -1, and to cancel it, we add 1 to make it 0.
+            return 1 + max(left, right)
+        
+        # Start the depth-first search from the root
+        dfs(root)
+        # Return the maximum diameter found during the traversal
+        return res
 
 
 
@@ -57,28 +83,3 @@ class Solution:
         
         # Return the max depth from the current node, that's if why 1 +. 1 is current node
         return 1 + max(left_depth, right_depth)
-
-
-
-
-
-
-
-
-
-
-
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
-        def depth(p):
-            if not p: return 0
-            left, right = depth(p.left), depth(p.right)
-            self.ans = max(self.ans, left+right)
-            return 1 + max(left, right)
-        depth(root)
-        return self.ans
-
-
-
-
