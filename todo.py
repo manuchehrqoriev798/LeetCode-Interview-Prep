6a3f8a0ec8d2e637e3b61ecced30d9f4b1a154ed
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2024-02-18 14:26:50 +0600

    updated

--- todo.py
+++ todo.py
@@ -12,42 +12,6 @@ I believe that I can do it !
 
 
 #TODO list:
+Yandex Preparation
 Open source contribution 
 
-
-
-
-1. Top-k Largest Elements
-2. Sliding Window
-3. Backtracking
-4. Dynamic Programming
-5. DFS and BFS
-6. Topological Sort
-7. Dijkstra's Algorithm
-
-
-
-
-# Solve 5 problems each:
-- Two Pointers
-- Binary Search
-- Sliding Window
-- Trees (DFS, BFS)
-- Merge Intervals
-- Backtracking
-- Stacks
-- Hash Maps
-- Heaps
-- Linked Lists, specifically creating new ones and reversing existing
-- Greedy
-- Fast and Slow Pointers
-- Two Heaps
-- K-way merge
-- Top K Elements
-- Subsets
-- Matrices
-- Topological Sort
-- Dynamic Programming
-- Tries
-- Union Find
-- Bitwise manipulation
