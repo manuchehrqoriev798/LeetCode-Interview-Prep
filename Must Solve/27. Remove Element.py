d5dccb0d67d27af6b601fbbd1656da46bb327537
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2024-02-22 02:49:26 +0600

    updated

---
+++ Must Solve/27. Remove Element.py
@@ -0,0 +1,34 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l, r = 0, 0
+
+        while r < len(nums):
+            if nums[l] == val and nums[r] != val:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            elif nums[l] != val:
+                l += 1
+
+            r += 1
+
+        return l 
+
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l, r = 0, 1
+
+        while r < len(nums):
+            if nums[l] == val and nums[r] != val:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            elif nums[l] != val:
+                l += 1
+
+            r += 1
+        
+        if l < len(nums) and nums[l] == val:
+            return l
+        else:
+            return l + 1
