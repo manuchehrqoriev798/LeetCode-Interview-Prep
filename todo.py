d53b9fa763f4e805246d164b9abab12323f4d6e3
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2024-04-30 12:03:50 +0600

    updated todo list

--- todo.py
+++ todo.py
@@ -1,20 +1,12 @@
 # Motivation is temporarily, have a discipline !
 Don't search for the right choice, make the choice right !!!
 
-# You should understand what happens if any line of code will be removed.
-
-
 
 
 
 
 #TODO list:
 
-NeetCode
-DP
-Graph
-Bit
+finish NeetCode List of problems
 
 Open source contribution 
-
-
