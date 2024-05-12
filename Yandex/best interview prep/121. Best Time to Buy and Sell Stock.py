130c7d653be5536016cd4bbe30688f63928ad3c5
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2024-05-12 11:09:48 +0600

    update

---
+++ Yandex/best interview prep/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        maxProfit = 0
+
+        l, r = 0, 1
+
+        while r < len(prices):
+            if prices[r] - prices[l] > 0:
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+                r += 1
+            else:
+                l = r
+                r += 1
+        
+        return maxProfit
+
+
+
+
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        l, r = 0, 0
+        maxProfit = 0
+
+
+        while r < len(prices):
+            if prices[r] - prices[l] > 0:
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+                r += 1
+            else:
+                l = r
+                r += 1
+        
+        return maxProfit
--- Yandex/best interview prep/238. Product of Array Except Self.py
+++ Yandex/best interview prep/238. Product of Array Except Self.py
@@ -1,22 +1,51 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        count = 0
+        total = 1
+        for num in nums:
+            if num == 0:
+                count += 1
+            else:
+                total *= num
+        
+        res = [1] * len(nums)
+        for i in range(len(nums)):
+            if nums[i] == 0 and count == 1:
+                res[i] = total
+            elif nums[i] != 0 and count > 0:
+                res[i] = 0
+            else:
+                if nums[i] != 0:
+                    res[i] = int(total / nums[i])
+                else:
+                    res[i] = 0
+
+
+        return res
+
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         zeros = 0
-        count = 1
+        total = 1
         for num in nums:
             if num == 0:
                 zeros += 1
             else:
-                count = count * num
+                total = total * num
 
         for i in range(len(nums)):
             if zeros == 0:
-                nums[i] = count // nums[i]
+                nums[i] = total // nums[i]
             elif zeros == 1 and nums[i] == 0:
-                nums[i] = count
+                nums[i] = total
             else:
                 nums[i] = 0
         
         return nums
+
 
 
 
